3801a425b4e4c8e56515e8c34915ed33867244f3
jjm6564 <jjm6564@naver.com>
2021-08-04 14:57:52 +0900

    Add templete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Templete from './JM/Templete';
 
 
 class App extends Component {
     render() {
         return (
             <div>
+            <Templete/>
             </div>
         );
     }
