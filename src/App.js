44fe372bae0fbf3ef66af10af6170e87e6ac5f1c
unknown <gdkjs@naver.com>
2021-08-05 23:18:10 +0900

    addMS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import INFO from './MS/info';
 
 class App extends Component {
     render() {
         return (
             <div>
+            <INFO />
             </div>
         );
     }
